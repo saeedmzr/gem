898b30cc99100f64c1e44595fe425c0e00810f87
saeed <saeedmozuarmi@gmail.com>
2022-06-18 20:33:01 +0430

    method login has been added to user Repository .

--- app/Repositories/User/UserRepository.php
+++ app/Repositories/User/UserRepository.php
@@ -4,11 +4,20 @@ namespace App\Repositories\User;
 
 use App\Models\User;
 use App\Repositories\BaseRepository;
+use Illuminate\Support\Facades\Hash;
 
 class UserRepository extends BaseRepository
 {
     public function __construct(User $model)
     {
         $this->model = $model;
+    }
+
+    public function login(string $email, $password)
+    {
+        $user = User::where('email', $email)->first();
+        if (!$user) return false;
+        if (!Hash::check($password, $user->password)) return false;
+        return $user;
     }
 }
