831efb55b981091ea0c4882c60a2b1ea0154a25a
saeed <saeedmozuarmi@gmail.com>
2022-06-18 20:47:45 +0430

    add seeder for user model .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,11 +15,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        \App\Models\User::factory(10)->create();
+        $this->seedPersonalDataForUser();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+    }
+
+    public function seedPersonalDataForUser()
+    {
+        User::create([
+            'name' => 'saeed',
+            'email' => 'saeedmouzarmi@gmail.com',
+            'password' => '11111111',
+        ]);
     }
 }
